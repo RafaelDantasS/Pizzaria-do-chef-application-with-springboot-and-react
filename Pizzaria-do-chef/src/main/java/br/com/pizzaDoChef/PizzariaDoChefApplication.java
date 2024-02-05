46901d9e5605351cf5a7e7a8e01fee8019d36b52
Rafael Dantas <rafadantasdasilva54@gmail.com>
2024-02-05 01:45:39 -0300

    informações de criação do projeto e arquetipo maven

---
+++ Pizzaria-do-chef/src/main/java/br/com/pizzaDoChef/PizzariaDoChefApplication.java
@@ -0,0 +1,13 @@
+package br.com.pizzaDoChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaDoChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaDoChefApplication.class, args);
+	}
+
+}
